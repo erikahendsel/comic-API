a2d5a3c503880acd3e26966c4722d49c1a9ef22e
erikahendsel <erikahendsel@hotmail.com>
2020-06-15 14:50:52 +0300

    fetching Marvel Data

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const { request } = require("express");
+const fetch = require("node-fetch");
+require("dotenv").config();
+const md5_hashing = require("./md5.js");
+const app = express();
+app.listen(3000, () => console.log("listening at 3000"));
+app.use(express.static("public"));
+
+console.log(process.env);
+
+//Get function from another file: https://www.stanleyulili.com/node/node-modules-learn-how-to-import-and-use-functions-from-another-file/
+
+const publickey = process.env.PUBLIC_API_KEY;
+const privatekey = process.env.PRIVATE_API_KEY;
+const ts = new Date().getTime();
+const stringToHash = ts + privatekey + publickey;
+const hash = md5_hashing.MD5(stringToHash);
+const baseUrl = "https://gateway.marvel.com:443/v1/public/characters";
+const limit = 20;
+const url =
+  baseUrl +
+  "?limit=" +
+  limit +
+  "&ts=" +
+  ts +
+  "&apikey=" +
+  publickey +
+  "&hash=" +
+  hash;
+console.log(url);
+app.get("/comic", async (request, response) => {
+  const api_url = `${url}`;
+  const fetch_response = await fetch(api_url);
+  const json = await fetch_response.json();
+  response.json(json);
+});
