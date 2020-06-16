54b1f57945e3448885680030056dfb62bc65d09a
erikahendsel <erikahendsel@hotmail.com>
2020-06-16 23:14:59 +0300

    C004 - Profile visual made

--- index.js
+++ index.js
@@ -12,26 +12,47 @@ app.use(express.static("public"));
 const publickey = process.env.PUBLIC_API_KEY;
 const privatekey = process.env.PRIVATE_API_KEY;
 const format = "hardcover";
+const orderBy = "-focDate";
 const ts = new Date().getTime();
 const stringToHash = ts + privatekey + publickey;
 const hash = md5_hashing.MD5(stringToHash);
 const baseUrl = "http://gateway.marvel.com/v1/public/comics";
-const limit = 100;
-const url =
+const mainLimit = 100;
+const latestComicsLimit = 3;
+const main_url =
   baseUrl +
   "?format=" +
   format +
   "&limit=" +
-  limit +
+  mainLimit +
   "&ts=" +
   ts +
   "&apikey=" +
   publickey +
   "&hash=" +
   hash;
-console.log(url);
+const latestComics_url =
+  baseUrl +
+  "?format=" +
+  format +
+  "&orderBy=" +
+  orderBy +
+  "&limit=" +
+  latestComicsLimit +
+  "&ts=" +
+  ts +
+  "&apikey=" +
+  publickey +
+  "&hash=" +
+  hash;
 app.get("/comic", async (request, response) => {
-  const api_url = `${url}`;
+  const api_url = `${main_url}`;
+  const fetch_response = await fetch(api_url);
+  const json = await fetch_response.json();
+  response.json(json);
+});
+app.get("/latest-comics", async (request, response) => {
+  const api_url = `${latestComics_url}`;
   const fetch_response = await fetch(api_url);
   const json = await fetch_response.json();
   response.json(json);
---
+++ public/scripts/home.js
@@ -0,0 +1,31 @@
+const allComics = document.querySelector(".latest-comics");
+async function latestComics() {
+  const latest_url = `/latest-comics`;
+  const latestResponse = await fetch(latest_url);
+  const latestComic_json = await latestResponse.json();
+  const comic_results = latestComic_json.data.results;
+  console.log(latestComic_json);
+  showLatestComics(comic_results);
+}
+latestComics();
+
+function showLatestComics(data) {
+  data.forEach((element) => {
+    const apiComicTitle = element.title;
+    const apiComicImages = element.images;
+    const singleComicContainer = document.createElement("div");
+    const webComicTitle = document.createElement("p");
+    const webComicImage = document.createElement("img");
+    singleComicContainer.classList.add("comic-container");
+    webComicTitle.classList.add("comic-title");
+    webComicTitle.textContent = apiComicTitle;
+    singleComicContainer.appendChild(webComicTitle);
+    apiComicImages.forEach((image) => {
+      //   console.log(image.path + "." + image.extension);
+      webComicImage.classList.add("comic-image");
+      webComicImage.src = `${image.path}.${image.extension}`;
+      singleComicContainer.appendChild(webComicImage);
+    });
+    allComics.appendChild(singleComicContainer);
+  });
+}
