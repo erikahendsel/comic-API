adad0ec8e5d4285b97702524af3498d3800148d1
erikahendsel <erikahendsel@hotmail.com>
2020-06-23 13:04:21 +0300

    C005 - WIP - onclick on comic opens an url wid comic ID

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,45 +1,56 @@
 const allComics = document.querySelector(".comics");
-const comicList = document.querySelector(".comic-container");
 
 async function getComics() {
   const api_url = `/comics`;
   const response = await fetch(api_url);
   const comic_json = await response.json();
   const comic_results = comic_json.data.results;
+  console.log(comic_json);
   showAllComics(comic_results);
 }
 
 function openComic(event) {
-  console.log(event.target);
+  console.log(event);
+  window.open(`comic-id.html?${event}`);
 }
 getComics();
-
-// async function main() {
-//   await getComics();
-//   await openComic();
-// }
-
-// main();
 
 function showAllComics(data) {
   data.forEach((element) => {
     const apiComicTitle = element.title;
     const apiComicImages = element.images;
+    const apiComicPrices = element.prices;
+    const apiComicID = element.id;
     const singleComicContainer = document.createElement("div");
+    const singleComicTextContainer = document.createElement("div");
     const webComicTitle = document.createElement("p");
     const webComicImage = document.createElement("img");
+    const webComicPrice = document.createElement("p");
+    singleComicTextContainer.classList.add("comic-text-container");
+
     apiComicImages.forEach((image) => {
       webComicImage.classList.add("comic-image");
       webComicImage.src = `${image.path}.${image.extension}`;
       singleComicContainer.appendChild(webComicImage);
     });
-    singleComicContainer.classList.add("comic-container");
+
+    singleComicContainer.classList.add("comic-container", `id-${apiComicID}`);
+    singleComicContainer.appendChild(singleComicTextContainer);
     webComicTitle.classList.add("comic-title");
     webComicTitle.textContent = apiComicTitle;
-    singleComicContainer.appendChild(webComicTitle);
+    singleComicTextContainer.appendChild(webComicTitle);
+
+    apiComicPrices.forEach((price) => {
+      webComicPrice.classList.add("comic-price");
+      webComicPrice.textContent = `price: ${price.price}`;
+      singleComicTextContainer.appendChild(webComicPrice);
+    });
 
     allComics.appendChild(singleComicContainer);
 
-    singleComicContainer.addEventListener("click", openComic);
+    singleComicContainer.addEventListener("click", function () {
+      openComic(apiComicID);
+    });
+    // singleComicContainer.addEventListener("click", openComic);
   });
 }
