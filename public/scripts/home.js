7c3c4b21145d5195702b9c6503f2ac2fabf384fc
erikahendsel <erikahendsel@hotmail.com>
2020-07-02 17:03:17 +0300

    C010: Decorated footer for mobile

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -15,3 +15,21 @@ function openComic(event) {
   return event;
 }
 getComics();
+
+// var controller = new ScrollMagic.Controller();
+// var scene = new ScrollMagic.Scene();
+
+// var scene = new ScrollMagic.Scene({
+//   triggerElement: "#trigger-div", // starting scene, when reaching this element
+//   duration: 400, // pin the element for a total of 400px
+//   triggerHook: "onEnter",
+// }).setPin("#trigger-div"); // the element we want to pin
+
+// // Add Scene to ScrollMagic Controller
+// controller.addScene(scene);
+
+// scene.on("add", function (event) {
+//   const triggerP = document.querySelector(".trigger-div");
+//   triggerP.textContent = "You entered the new scene!!!!";
+//   console.log("added");
+// });
